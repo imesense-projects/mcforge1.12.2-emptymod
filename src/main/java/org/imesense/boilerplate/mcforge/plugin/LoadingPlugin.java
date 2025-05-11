7bd26c5ea2b25b4afc4c35d84ca9f4ccad096a20
OldSerpskiStalker <ictopiar@gmail.com>
2025-05-11 12:42:02 +0300

    Fix build and launch

--- src/main/java/org/imesense/boilerplate/mcforge/plugin/LoadingPlugin.java
+++ src/main/java/org/imesense/boilerplate/mcforge/plugin/LoadingPlugin.java
@@ -3,6 +3,8 @@ package org.imesense.boilerplate.mcforge.plugin;
 import java.io.File;
 import java.util.Map;
 
+import org.slf4j.LoggerFactory;
+
 import net.minecraftforge.common.ForgeVersion;
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
 
@@ -17,6 +19,8 @@ import org.imesense.boilerplate.mcforge.transformer.ClassTransformer;
 @IFMLLoadingPlugin.SortingIndex(LoadingPlugin.AFTER_DEOBFUSCATION)
 public final class LoadingPlugin implements IFMLLoadingPlugin
 {
+    private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(LoadingPlugin.class);
+
     public static File FileLocation;
     public static Boolean RuntimeDeobfuscation;
 
@@ -24,13 +28,29 @@ public final class LoadingPlugin implements IFMLLoadingPlugin
 
     public LoadingPlugin()
     {
-        MixinBootstrap.init();
-        Mixins.addConfiguration("mixins.boilerplatemcforge.json");
+        LOGGER.info("Initializing LoadingPlugin");
+
+        try
+        {
+            LOGGER.debug("Initializing Mixin");
+
+            MixinBootstrap.init();
+            Mixins.addConfiguration("mixins.boilerplatemcforge.json");
+
+            LOGGER.info("Mixin initialization complete");
+        }
+        catch (Exception exception)
+        {
+            LOGGER.error("Failed to initialize Mixin", exception);
+            throw exception;
+        }
     }
 
     @Override
     public String[] getASMTransformerClass()
     {
+        LOGGER.debug("Providing ASM transformer classes");
+
         return new String[]
         {
             ClassTransformer.class.getName()
@@ -40,34 +60,56 @@ public final class LoadingPlugin implements IFMLLoadingPlugin
     @Override
     public String getModContainerClass()
     {
+        LOGGER.trace("getModContainerClass() called - returning null");
+
         return null;
     }
 
     @Override
     public String getSetupClass()
     {
+        LOGGER.trace("getSetupClass() called - returning null");
+
         return null;
     }
 
     @Override
     public void injectData(Map<String, Object> objectMap)
     {
-        RuntimeDeobfuscation = (Boolean) objectMap.get("runtimeDeobfuscationEnabled");
-        FileLocation = (File) objectMap.get("coremodLocation");
-        if (FileLocation == null)
+        LOGGER.debug("Injecting coremod data");
+
+        try
         {
-            FileLocation = new File(
-                getClass()
-                    .getProtectionDomain()
-                    .getCodeSource()
-                    .getLocation()
-                    .getPath());
+            RuntimeDeobfuscation = (Boolean) objectMap.get("runtimeDeobfuscationEnabled");
+            LOGGER.debug("Runtime deobfuscation enabled: {}", RuntimeDeobfuscation);
+
+            FileLocation = (File) objectMap.get("coremodLocation");
+            if (FileLocation == null)
+            {
+                LOGGER.debug("coremodLocation not provided, determining from protection domain");
+
+                FileLocation = new File(
+                    getClass()
+                        .getProtectionDomain()
+                        .getCodeSource()
+                        .getLocation()
+                        .getPath());
+            }
+            LOGGER.debug("Coremod location: {}", FileLocation);
+        }
+        catch (Exception exception)
+        {
+            LOGGER.error("Error during data injection", exception);
+
+            throw exception;
         }
     }
 
     @Override
     public String getAccessTransformerClass()
     {
+        LOGGER.trace("getAccessTransformerClass() called - returning null");
+
         return null;
     }
 }
