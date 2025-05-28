42c682c39115be14b54e68f9b3cbca09eab4e535
OldSerpskiStalker <ictopiar@gmail.com>
2025-05-28 22:39:19 +0300

    Rework mixins loading mechanism
    Co-authored-by: acidicMercury8 <acidicmercury8@outlook.com>

---
+++ src/main/java/org/imesense/boilerplate/mcforge/mixin/MixinEntityPlayer.java
@@ -0,0 +1,19 @@
+package org.imesense.boilerplate.mcforge.mixin;
+
+import net.minecraft.entity.player.EntityPlayer;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(EntityPlayer.class)
+public abstract class MixinEntityPlayer
+{
+    @Inject(method = "onUpdate", at = @At("HEAD"))
+    private void onPlayerUpdate(CallbackInfo callbackInfo)
+    {
+        EntityPlayer player = (EntityPlayer) (Object) this;
+        System.out.println("Player " + player.getName() + " is updating!");
+    }
+}
