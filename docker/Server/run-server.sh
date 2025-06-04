e50b33d191ac888446f910a5277dfb96ac725a38
acidicMercury8 <acidicmercury8@outlook.com>
2025-06-05 00:58:35 +0300

    Add Docker server manifest

---
+++ docker/Server/run-server.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+if [ "$EULA" = "true" ]; then
+    echo "eula=true" > eula.txt
+else
+    echo "You need to accept the EULA by setting EULA=true"
+    exit 1
+fi
+
+java \
+    -Xms${MIN_MEMORY} \
+    -Xmx${MAX_MEMORY} \
+    -jar forge.jar nogui
