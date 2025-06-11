a565a16572db8020217df16ab05455c0a6cec6ae
acidicMercury8 <acidicmercury8@outlook.com>
2025-06-11 22:11:59 +0300

    Fix Docker related issues

--- docker/Builder/build-mod.bat
+++ docker/Builder/build-mod.bat
@@ -3,6 +3,9 @@
 :: Build mod
 docker build ^
     --file docker\Builder\Dockerfile ^
+    --progress=plain ^
     --target export ^
     --output type=local,dest=. ^
+    --cache-to type=local,dest=.\.docker\cache ^
+    --cache-from type=local,src=.\.docker\cache ^
     .
