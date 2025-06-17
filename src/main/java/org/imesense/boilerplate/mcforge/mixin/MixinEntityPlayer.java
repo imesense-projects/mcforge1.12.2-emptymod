7136af7ebc42908a707a8b0a37dc7a70b03f6eb9
acidicMercury8 <acidicmercury8@outlook.com>
2025-06-17 23:43:30 +0300

    Add missing documenting comments
    Co-authored-by: OldSerpskiStalker <ictopiar@gmail.com>

--- src/main/java/org/imesense/boilerplate/mcforge/mixin/MixinEntityPlayer.java
+++ src/main/java/org/imesense/boilerplate/mcforge/mixin/MixinEntityPlayer.java
@@ -7,9 +7,27 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+/**
+ * Mixin for {@link EntityPlayer} class.
+ * <p>
+ * Adds additional logic during player state updates (when {@code onUpdate} is called).
+ *
+ * @see Mixin
+ * @see EntityPlayer
+ */
 @Mixin(EntityPlayer.class)
 public abstract class MixinEntityPlayer
 {
+    /**
+     * Intercepts the {@link EntityPlayer#onUpdate()} method call to perform additional actions.
+     * <p>
+     * Current implementation prints a player update notification to console.
+     *
+     * @param callbackInfo Callback information for controlling Mixin injection
+     *
+     * @see Inject
+     * @see CallbackInfo
+     */
     @Inject(method = "onUpdate", at = @At("HEAD"))
     private void onPlayerUpdate(CallbackInfo callbackInfo)
     {
