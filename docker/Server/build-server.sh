0366a9368e27f9610bba9326e9457dd073bbd68b
acidicMercury8 <acidicmercury8@outlook.com>
2025-06-17 19:43:22 +0300

    Fix server Docker image

---
+++ docker/Server/build-server.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Build server
+docker build \
+    --file docker/Server/Dockerfile \
+    --progress=plain \
+    --target final \
+    --tag mcforge1.12.2-modification-boilerplate:latest \
+    --tag ghcr.io/imesense/mcforge1.12.2-modification-boilerplate:latest \
+    .
