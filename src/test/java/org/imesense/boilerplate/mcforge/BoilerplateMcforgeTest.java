457b9136bdd47f7004e75e1738ea48f0f6042b88
acidicMercury8 <acidicmercury8@outlook.com>
2025-07-02 14:25:48 +0300

    Fix tests naming

--- src/test/java/org/imesense/boilerplate/mcforge/BoilerplateMcforgeTest.java
+++ src/test/java/org/imesense/boilerplate/mcforge/BoilerplateMcforgeTest.java
@@ -30,6 +30,7 @@ import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 @MockitoSettings(strictness = Strictness.LENIENT)
+@SuppressWarnings("LoggingSimilarMessage")
 public class BoilerplateMcforgeTest
 {
     private static final String CLASS_NAME = "org.imesense.boilerplate.mcforge.BoilerplateMcforge";
@@ -68,7 +69,7 @@ public class BoilerplateMcforgeTest
 
         try
         {
-            Field field = BoilerplateMcforge.class.getDeclaredField("LOGGER");
+            Field field = BoilerplateMcforge.class.getDeclaredField("logger");
             field.setAccessible(true);
             field.set(null, mockLogger);
         }
@@ -81,41 +82,24 @@ public class BoilerplateMcforgeTest
     }
 
     @Test
-    public void testClassConstants_SetsCorrect()
+    public void boilerplateMcforge_Constants_SetsCorrect()
     {
-        assertEquals("boilerplatemcforge", BoilerplateMcforge.MODID);
+        assertEquals("boilerplatemcforge", BoilerplateMcforge.MOD_ID);
         assertEquals("Minecraft Forge Modification Boilerplate", BoilerplateMcforge.NAME);
         assertEquals("1.12.2-14.23.5.2860", BoilerplateMcforge.VERSION);
     }
 
     @Test
-    public void testPreInit_ClientSideLogsError()
+    public void boilerplateMcforge_logMethodCall_LogsCorrect() throws Exception
     {
-        when(mockPreInitEvent.getSide()).thenReturn(Side.CLIENT);
-
-        try (MockedStatic<I18n> mockedI18n = mockStatic(I18n.class))
-        {
-            mockedI18n.when(() -> I18n.format(anyString())).thenReturn("test");
-
-            boilerplateMcforge.preInit(mockPreInitEvent);
-
-            verify(mockLogger).error("No mixin config files found!");
-        }
+        Method method = BoilerplateMcforge.class.getDeclaredMethod("logMethodCall", String.class);
+        method.setAccessible(true);
+        method.invoke(boilerplateMcforge, "testMethod");
+        verify(mockLogger).info(LOG_MESSAGE, CLASS_NAME, "testMethod");
     }
 
     @Test
-    public void testPreInit_ServerSideLogsCorrect()
-    {
-        when(mockPreInitEvent.getSide()).thenReturn(Side.SERVER);
-
-        boilerplateMcforge.preInit(mockPreInitEvent);
-
-        verify(mockLogger).info(LOG_MESSAGE, CLASS_NAME, "preInit");
-        verifyNoMoreInteractions(mockLogger);
-    }
-
-    @Test
-    public void testSetLocaleMetadata_SetsCorrect()
+    public void boilerplateMcforge_setLocaleMetadata_SetsCorrect()
     {
         try (MockedStatic<I18n> mockedI18n = mockStatic(I18n.class))
         {
@@ -133,46 +117,63 @@ public class BoilerplateMcforgeTest
     }
 
     @Test
-    public void testInit_CallsCorrect()
+    public void boilerplateMcforge_preInit_ClientSideLogsError()
+    {
+        when(mockPreInitEvent.getSide()).thenReturn(Side.CLIENT);
+
+        try (MockedStatic<I18n> mockedI18n = mockStatic(I18n.class))
+        {
+            mockedI18n.when(() -> I18n.format(anyString())).thenReturn("test");
+
+            boilerplateMcforge.preInit(mockPreInitEvent);
+
+            verify(mockLogger).error("No mixin config files found!");
+        }
+    }
+
+    @Test
+    public void boilerplateMcforge_preInit_ServerSideLogsCorrect()
+    {
+        when(mockPreInitEvent.getSide()).thenReturn(Side.SERVER);
+
+        boilerplateMcforge.preInit(mockPreInitEvent);
+
+        verify(mockLogger).info(LOG_MESSAGE, CLASS_NAME, "preInit");
+        verifyNoMoreInteractions(mockLogger);
+    }
+
+    @Test
+    public void boilerplateMcforge_init_CallsCorrect()
     {
         boilerplateMcforge.init(mockInitEvent);
         verify(mockLogger).info(LOG_MESSAGE, CLASS_NAME, "init");
     }
 
     @Test
-    public void testPostInit_CallsCorrect()
+    public void boilerplateMcforge_postInit_CallsCorrect()
     {
         boilerplateMcforge.postInit(mockPostInitEvent);
         verify(mockLogger).info(LOG_MESSAGE, CLASS_NAME, "postInit");
     }
 
     @Test
-    public void testOnLoadComplete_CallsCorrect()
+    public void boilerplateMcforge_onLoadComplete_CallsCorrect()
     {
         boilerplateMcforge.onLoadComplete(mockLoadCompleteEvent);
         verify(mockLogger).info(LOG_MESSAGE, CLASS_NAME, "onLoadComplete");
     }
 
     @Test
-    public void testServerLoad_CallsCorrect()
+    public void boilerplateMcforge_serverLoad_CallsCorrect()
     {
         boilerplateMcforge.serverLoad(mockServerStartingEvent);
         verify(mockLogger).info(LOG_MESSAGE, CLASS_NAME, "serverLoad");
     }
 
     @Test
-    public void testServerStopped_CallsCorrect()
+    public void boilerplateMcforge_serverStopped_CallsCorrect()
     {
         boilerplateMcforge.serverStopped(mockServerStoppedEvent);
         verify(mockLogger).info(LOG_MESSAGE, CLASS_NAME, "serverStopped");
-    }
-
-    @Test
-    public void testLogMethodCall_LogsCorrect() throws Exception
-    {
-        Method method = BoilerplateMcforge.class.getDeclaredMethod("logMethodCall", String.class);
-        method.setAccessible(true);
-        method.invoke(boilerplateMcforge, "testMethod");
-        verify(mockLogger).info(LOG_MESSAGE, CLASS_NAME, "testMethod");
     }
 }
