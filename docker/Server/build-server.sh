4d9925d287ef29f4ee729161d20eab5881374277
acidicMercury8 <acidicmercury8@outlook.com>
2026-01-04 22:37:27 +0300

    Update server Docker image name and tag

--- docker/Server/build-server.sh
+++ docker/Server/build-server.sh
@@ -5,6 +5,6 @@ docker build \
     --file docker/Server/Dockerfile \
     --progress=plain \
     --target final \
-    --tag mcforge1.12.2-modification-boilerplate:latest \
-    --tag ghcr.io/imesense/mcforge1.12.2-modification-boilerplate:latest \
+    --tag mcforge1.12.2-modification-boilerplate-server:latest \
+    --tag ghcr.io/imesense/mcforge1.12.2-modification-boilerplate-server:latest \
     .
