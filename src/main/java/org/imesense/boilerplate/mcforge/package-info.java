3cec2a0bdf35c7cb204f090b978eb21b772b2ec5
acidicMercury8 <acidicmercury8@outlook.com>
2026-07-05 17:25:30 +0300

    Add missing Java package info manifest
    Assisted-by: Koda:koda-pro

---
+++ src/main/java/org/imesense/boilerplate/mcforge/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * Main package for the Minecraft Forge 1.12.2 mod boilerplate.
+ * <p>
+ * Provides a base structure for creating modifications with Mixin support via
+ * {@link fermiumbooter.FermiumRegistryAPI}.
+ * </p>
+ * <p>
+ * Dependencies: requires {@code fermiumbooter}.
+ * </p>
+ * @since 1.12.2-14.23.5.2864
+ */
+package org.imesense.boilerplate.mcforge;
